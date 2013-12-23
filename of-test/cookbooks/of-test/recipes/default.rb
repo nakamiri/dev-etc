ed6723a10131eba81733dfd4d56aec585ab7778a
nakamiri <nakamiri@gmail.com>
2013-12-23 22:39:42 +0900

    initial

--- of-test/cookbooks/of-test/recipes/default.rb
+++ of-test/cookbooks/of-test/recipes/default.rb
@@ -1,13 +1,65 @@
-# libpcap-dev, libsqlite3-dev
+execute "apt-get update" do
+  command "apt-get update"
+end
+
+package "erlang" do
+  action :install
+end
+
+package "gcc" do
+  action :install
+end
+
+package "openssl" do
+  action :install
+end
+
+package "libssl0.9.8" do
+  action :install
+end
+
+package "libssl-dev" do
+  action :install
+end
+
+package "libncurses5" do
+ action :install
+end
+
+package "libncurses5-dev" do
+  action :install
+end
+
+package "git-core" do
+  action :install
+end
+
+package "bridge-utils" do
+  action :install
+end
+
+package "libpcap0.8" do
+  action :install
+end
+
 package "libpcap-dev" do
   action :install
 end
 
-package "libsqlite3-dev" do
+package "libcap2-bin" do
   action :install
 end
 
-gem_package "trema" do
+package "uml-utilities" do
   action :install
 end
+
+#package "libsqlite3-dev" do
+#  action :install
+#end
+
+#gem_package "trema" do
+#  action :install
+#end
+
 
